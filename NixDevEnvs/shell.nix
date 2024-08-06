8f9242e6e67e0550cad8080e2fb8e03c21d06b3f
Youssef <siryoussef@hotmail.com>
2024-08-06 14:36:02 +0300

    adding development enviroment folder to nixos repo
    for future integration into nixos flake & for current convenience

---
+++ NixDevEnvs/shell.nix
@@ -0,0 +1,58 @@
+ # shell.nix
+let
+  pkgs = import <nixpkgs> {};
+  python = pkgs.python3;
+  pythonPackages = python.pkgs;
+in
+
+with pkgs;
+
+mkShell {
+  name = "pip-env";
+  buildInputs = with pythonPackages; [
+    azure-cli
+    kubectl
+
+    # Python requirements (enough to get a virtualenv going).
+    psutil
+    #tensorflow
+    #pyarrow
+    pandas
+    matplotlib
+    seaborn
+    scikit-learn
+    tensorflow
+    keras
+    ipykernel
+    jupyter
+    pytest
+    setuptools
+    wheel
+    venvShellHook
+
+    libffi
+    openssl
+    gcc
+    unzip
+#     ipython-sql
+#     sqlalchemy_1_4
+#     pymysql
+  ];
+  venvDir = "venv3";
+  src = null;
+  postVenv = ''
+    unset SOURCE_DATE_EPOCH
+    ./scripts/install_local_packages.sh
+  '';
+  postShellHook = ''
+    # Allow the use of wheels.
+    unset SOURCE_DATE_EPOCH
+
+    # get back nice looking PS1
+    source ~/.bashrc
+    source <(kubectl completion bash)
+
+    PYTHONPATH=$PWD/$venvDir/${python.sitePackages}:$PYTHONPATH
+  '';
+}
+
